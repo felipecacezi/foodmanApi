5c6f77f2b5d17726a43ee9735127fe752c2f85af
Felipe Cacezi <felipecacezi@gmail.com>
2023-11-28 07:10:15 -0300

    separando rotas em arquivos diferentes - criação do arquivo de rotas de itens

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,11 +30,11 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+                ->prefix('api/v1/itens')
+                ->group(base_path('routes/api/itens.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            // Route::middleware('web')
+            //     ->group(base_path('routes/web.php'));
         });
     }
 }
